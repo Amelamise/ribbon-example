c4e51099e9fb1d2570b53b4af4b8e53f443425b7
Dean Gottschalk <dean.gottschalk@magnetek.com>
2015-09-23 11:10:33 -0500

    Initial commit

---
+++ NegativeIntegerConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Ribbon_Example
+{
+    class NegativeIntegerConverter : IValueConverter
+    {
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            return -1 * System.Convert.ToInt32( value );
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            return -1 * System.Convert.ToInt32( value );
+        }
+    }
+}
